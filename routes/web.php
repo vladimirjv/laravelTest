3b2e09d1949e4c9598c172505f90ed5ce96a100d
vladimirjv <ivladimirjuarez@outlook.com>
2019-11-29 13:56:09 -0600

    Init routes

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', "PaginasController@inicio");
 
 Route::get('/cualquiera', function() {
     return "pagina cualquiera";
