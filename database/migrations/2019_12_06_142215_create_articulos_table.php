0fa37a335a376f758c2c1a2053e76f508bb0cb11
vladimirjv <ivladimirjuarez@outlook.com>
2019-12-06 08:42:03 -0600

    articles db

---
+++ database/migrations/2019_12_06_142215_create_articulos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticulosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Description');
+            $table->decimal('Price');
+            $table->string('CountryOrigin');
+            $table->text('Observations');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
